8f4231c496292c50e2e6b8bf0e8ce46f961b8391
Thai Hac Nguyen <hac.nguyen@skale.asia>
2023-05-24 12:26:26 +0700

    skale/master: update type header + text

--- src/lib/CellTemplates/HeaderCellTemplate.tsx
+++ src/lib/CellTemplates/HeaderCellTemplate.tsx
@@ -1,32 +1,61 @@
-import * as React from 'react';
+import * as React from "react";
 
 // NOTE: all modules imported below may be imported from '@silevis/reactgrid'
-import { getCellProperty } from '../Functions/getCellProperty';
-import { Cell, CellStyle, CellTemplate, Compatible, Span, Uncertain } from '../Model/PublicModel';
-
+import { getCellProperty } from "../Functions/getCellProperty";
+import {
+  Cell,
+  CellStyle,
+  CellTemplate,
+  Compatible,
+  Span,
+  Uncertain,
+} from "../Model/PublicModel";
 
 export interface HeaderCell extends Cell, Span {
-    type: 'header';
-    text: string;
+  type: "header";
+  text: string;
+  required?: boolean;
 }
 
 export class HeaderCellTemplate implements CellTemplate<HeaderCell> {
+  getCompatibleCell(
+    uncertainCell: Uncertain<HeaderCell>
+  ): Compatible<HeaderCell> {
+    const text = getCellProperty(uncertainCell, "text", "string");
+    const value = parseFloat(text);
 
-    getCompatibleCell(uncertainCell: Uncertain<HeaderCell>): Compatible<HeaderCell> {
-        const text = getCellProperty(uncertainCell, 'text', 'string');
-        const value = parseFloat(text);
-        return { ...uncertainCell, text, value };
+    let required: boolean;
+    try {
+      required = getCellProperty(uncertainCell, "required", "boolean");
+    } catch {
+      required = false;
     }
 
-    render(cell: Compatible<HeaderCell>, isInEditMode: boolean, onCellChanged: (cell: Compatible<HeaderCell>, commit: boolean) => void): React.ReactNode {
-        return cell.text;
-    }
+    return { ...uncertainCell, text, value, required };
+  }
 
-    isFocusable = (cell: Compatible<HeaderCell>): boolean => false;
+  render(
+    cell: Compatible<HeaderCell>,
+    isInEditMode: boolean,
+    onCellChanged: (cell: Compatible<HeaderCell>, commit: boolean) => void
+  ): React.ReactNode {
+    return (
+      <>
+        <span>{cell.text}</span>
+        {cell.required && (
+          <span style={{ color: "red", marginLeft: 4 }}>(*)</span>
+        )}
+      </>
+    );
+  }
 
-    getClassName(cell: Compatible<HeaderCell>, isInEditMode: boolean): string {
-        return cell.className ? cell.className : '';
-    }
+  isFocusable = (cell: Compatible<HeaderCell>): boolean => false;
 
-    getStyle = (cell: Compatible<HeaderCell>): CellStyle => ({ background: 'rgba(128, 128, 128, 0.1)' })
+  getClassName(cell: Compatible<HeaderCell>, isInEditMode: boolean): string {
+    return cell.className ? cell.className : "";
+  }
+
+  getStyle = (cell: Compatible<HeaderCell>): CellStyle => ({
+    background: "rgba(128, 128, 128, 0.1)",
+  });
 }
